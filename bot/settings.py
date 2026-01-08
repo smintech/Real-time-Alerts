3ee768365e623af057c38e80cd0245c0096c1b42
Sm in tech <ellaelearnor66@gmail.com>
2026-01-08 15:20:26 +0100

    Update settings.py

--- bot/settings.py
+++ bot/settings.py
@@ -1,6 +1,6 @@
 import pytz
 
-TIMEZONE = pytz.timezone('Africa/Lagos')
+TIMEZONE = pytz.timezone('Africa/Nigeria')
 DEAL_DROP_THRESHOLD = 10  # % for high score
 MAX_WATCHES_FREE = 5      # Limit for MVP
 CHECK_INTERVAL = 3600     # 1 hour
