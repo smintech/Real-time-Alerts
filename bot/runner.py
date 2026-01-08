68e47130929bebea9095fbe890686479bca8c7d2
Sm in tech <ellaelearnor66@gmail.com>
2026-01-08 15:24:54 +0100

    Update runner.py

--- bot/runner.py
+++ bot/runner.py
@@ -1,1 +1,17 @@
+from fastapi import FastAPI
+from bot.bot import run_bot
+import multiprocessing
 
+app = FastAPI()
+
+@app.get("/")
+def home(): return {"status": "API Online"}
+
+if __name__ == "__main__":
+    # Start Bot in a separate process
+    p = multiprocessing.Process(target=run_bot)
+    p.start()
+    
+    # Start API
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
