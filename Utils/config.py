ecacca2dbb2d00a2a01d259dc5a3379c198912c8
Sm in tech <ellaelearnor66@gmail.com>
2026-01-10 00:49:48 +0100

    Update config.py

--- Utils/config.py
+++ Utils/config.py
@@ -7,5 +7,5 @@ TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 APIFY_TOKEN = os.getenv('APIFY_TOKEN')
 DB_URL = os.getenv('DB_URL', 'postgresql://user:pass@localhost/naija_db')
 REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
-
-SUPPORTED_SITES = ['jumia.ng', 'konga.com']
+ADMIN_IDS = [123456789]
+SUPPORTED_SITES = ['Julia.ng', 'konga.com']
