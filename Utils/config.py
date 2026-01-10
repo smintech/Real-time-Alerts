7c6475030767f20387b455f09f5c1d721a7940b1
Sm in tech <ellaelearnor66@gmail.com>
2026-01-10 21:33:26 +0100

    Update config.py

--- Utils/config.py
+++ Utils/config.py
@@ -5,8 +5,8 @@ load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 APIFY_TOKEN = os.getenv('APIFY_TOKEN')
-DB_URL = os.getenv('DB_URL', 'postgresql://user:pass@localhost/naija_db')
-REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
+DB_URL = os.getenv('DB_URL')
+REDIS_URL = os.getenv('REDIS_URL')
 ADMIN_IDS = os.getenv(ADMIN_IDS)
 SUPPORTED_SITES = ['Jumia.ng', 'konga.com','binance.com']
 SITE_ACTOR_MAP = {
