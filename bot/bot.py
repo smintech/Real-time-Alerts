a295c8565c0aff2bb052c4d8397637edf7d8dd8a
Sm in tech <ellaelearnor66@gmail.com>
2026-01-14 13:23:26 +0100

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -536,7 +536,7 @@ async def run_bot():
         application.job_queue.run_repeating(
             callback=check_and_post_channel_deals,
             interval=CHECK_INTERVAL_SECONDS,
-            first=90,
+            first=10,
             name="channel_deals"
         )
         application.job_queue.run_repeating(
