a1e76a911223e043375030f2d23864186652633b
Sm in tech <ellaelearnor66@gmail.com>
2026-01-14 17:58:45 +0100

    Update config.py

--- Utils/config.py
+++ Utils/config.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-APIFY_TOKEN = os.getenv('APIFY_TOKEN')
+#APIFY_TOKEN = os.getenv('APIFY_TOKEN')
 DB_URL = os.getenv('DB_URL')
 REDIS_URL = os.getenv('REDIS_URL')
 ADMIN_IDS = [int(x.strip()) for x in os.getenv("ADMIN_IDS", "").split(",") if x.strip()]
-SITE_ACTOR_MAP = {
+#SITE_ACTOR_MAP = {
     #'jumia.com.ng': 'fatihtahta/jumia-scraper'
-}
+#}
