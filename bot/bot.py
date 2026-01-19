b25f45c53f109d9a8f8cdc3c04021d4464e52766
Sm in tech <ellaelearnor66@gmail.com>
2026-01-19 23:02:01 +0100

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -858,7 +858,7 @@ async def run_bot():
                 callback=check_and_post_fuel_prices,
                 interval=120,
                 #time=dt_time(hour=7, minute=0, second=0, tzinfo=TIMEZONE),
-                name="check_and_post_fuel_prices",
+                name="check_fuel_prices",
                 first=10,
             )
             application.job_queue.run_repeating(
