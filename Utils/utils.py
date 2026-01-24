e6c5c301892233e295e472a10d0aa77c9be4572a
Sm in tech <ellaelearnor66@gmail.com>
2026-01-24 17:12:20 +0100

    Update utils.py

--- Utils/utils.py
+++ Utils/utils.py
@@ -1239,7 +1239,7 @@ for a in target_soup.find_all("a", href=True):
         item = {
             "title": link_text,
             "snippet": "",
-            "date": None, js,
+            "date": None,
             "link": full_link,
             "origin": "link",
             "detail_available": False,
