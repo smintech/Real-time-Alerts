1b2f077f8cd79fc08db30a07c4d4699b94a88525
Sm in tech <ellaelearnor66@gmail.com>
2026-02-10 20:11:47 +0100

    Update settings.py

--- bot/settings.py
+++ bot/settings.py
@@ -111,7 +111,7 @@ DEFAULT_SCHOOL_SOURCES = {
         "https://myschool.ng/news"
     ],
     "Punch Education": [
-        "https://punchng.com"
+        "https://punchng.com/topics/education/"
     ],
 
     #"Lagos State Education": [
