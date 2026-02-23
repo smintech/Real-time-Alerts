1eda274d15eda52452e95f5a8e5e933ee3b02e0a
Sm in tech <ellaelearnor66@gmail.com>
2026-02-23 08:28:17 +0100

    Update browser.py

--- Utils/browser.py
+++ Utils/browser.py
@@ -403,7 +403,7 @@ class SharedPlaywrightManager:
             'User-Agent': random.choice(self._cfg["user_agent_list"]),
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
             'Accept-Language': 'en-US,en;q=0.9,en-GB;q=0.8',
-            'Accept-Encoding': 'gzip, deflate, br, zstd',
+            'Accept-Encoding': 'gzip, deflate',
             'Cache-Control': 'max-age=0',
             'Connection': 'keep-alive',
             'DNT': '1',
